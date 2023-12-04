63c7134597863cd84e2d2ae050eb8ce8764c8356
Marco Peressutti <marco.eugenio.peressutti@gmail.com>
2023-12-04 14:13:57 +0100

    [day08] Implemented solution and tests for day 08 of AoC

--- src/solutions/day08.cpp
+++ src/solutions/day08.cpp
@@ -1,21 +1,23 @@
 #include "aoc/solutions/day08.hpp"
-
 #include "aoc/utils/aliases.hpp"
 #include "aoc/utils/parse.hpp"
+#include <algorithm>
+#include <iostream>
 
 struct Grid
 {
-  std::size_t   height{0};
-  std::size_t   width{0};
-  std::uint8_t *data{nullptr};
+  std::size_t  height{0};
+  std::size_t  width{0};
+  std::size_t *data{nullptr};
+  Grid() = default;
   explicit Grid(const aoc::vstring &input)
   {
     this->height = input.size();
     this->width  = input[0].size();
-    this->data   = new std::uint8_t[this->width * this->height];
+    this->data   = new std::size_t[this->width * this->height];
     for (std::size_t i = 0; i < this->height; ++i)
       for (std::size_t j = 0; j < this->width; ++j)
-        this->data[i * this->width + j] = std::uint8_t(input[i][j] - '0');
+        this->data[i * this->width + j] = std::size_t(input[i][j] - '0');
   }
 
   ~Grid() { delete[] this->data; }
@@ -24,6 +26,74 @@ struct Grid
 
   std::size_t visible(const std::size_t &x, const std::size_t &y) const
   {
+    const std::size_t tree_height = this->data[y * this->width + x];
+    std::size_t       temp        = this->data[x];
+    for (std::size_t i = 1; i < y; ++i)
+      if (this->data[i * this->width + x] > temp)
+        temp = this->data[i * this->width + x];
+    if (temp < tree_height) return 1;
+
+    temp = this->data[x + (y + 1) * this->width];
+    for (std::size_t i = y + 2; i < this->height; ++i)
+      if (this->data[i * this->width + x] > temp)
+        temp = this->data[i * this->width + x];
+    if (temp < tree_height) return 1;
+
+    temp = this->data[y * this->width];
+    for (std::size_t i = 1; i < x; ++i)
+      if (this->data[y * this->width + i] > temp)
+        temp = this->data[y * this->width + i];
+    if (temp < tree_height) return 1;
+
+    temp = this->data[y * this->width + x + 1];
+    for (std::size_t i = x + 2; i < this->width; ++i)
+      if (this->data[y * this->width + i] > temp)
+        temp = this->data[y * this->width + i];
+    if (temp < tree_height) return 1;
+    return 0;
+  }
+
+  std::size_t scenic_score(const std::size_t &x, const std::size_t &y) const
+  {
+    std::size_t       a = 0, b = 0, c = 0, d = 0;
+    const std::size_t tree_height = this->data[y * this->width + x];
+    std::size_t       i;
+    if (y != 0)
+    {
+      for (i = y - 1; i != 0; --i)
+      {
+        a++;
+        if (this->data[i * this->width + x] >= tree_height) break;
+      }
+      // if (this->data[x] <= tree_height && i == 0) a++;
+      if (i == 0) a++;
+    }
+
+    for (std::size_t i = y + 1; i < this->height; ++i)
+    {
+      b++;
+      if (this->data[i * this->width + x] >= tree_height) break;
+    }
+
+    if (x != 0)
+    {
+      for (std::size_t i = x - 1; i != 0; --i)
+      {
+        c++;
+        if (this->data[y * this->width + i] >= tree_height) break;
+      }
+      // if (this->data[x] <= tree_height) c++;
+      if (i == 0) c++;
+    }
+
+    for (std::size_t i = x + 1; i < this->width; ++i)
+    {
+      d++;
+      if (this->data[y * this->width + i] >= tree_height) break;
+    }
+
+    // up down left right
+    return a * b * c * d;
   }
 };
 
@@ -41,8 +111,19 @@ std::size_t aoc::day08::part1(const std::string &filename)
 
 std::size_t aoc::day08::part2(const std::string &filename)
 {
-  aoc::vstring input  = aoc::parse::cvt_file_to_vstring(filename);
-  std::size_t  result = 0;
+  aoc::vstring input = aoc::parse::cvt_file_to_vstring(filename);
+  Grid         grid(input);
+  Grid         visibility;
+  std::size_t  size = grid.height * grid.width;
+  visibility.height = grid.height;
+  visibility.width  = grid.width;
+  visibility.data   = new std::size_t[size]();
+  for (std::size_t y = 0; y < visibility.height; ++y)
+    for (std::size_t x = 0; x < visibility.width; ++x)
+      visibility.data[x + visibility.width * y] = grid.scenic_score(x, y);
 
-  return result;
+  std::size_t *result = std::max_element(visibility.data, visibility.data + size);
+  std::cerr << result - visibility.data << "\n";
+
+  return *result;
 }
--- test/src/day08-test.cpp
+++ test/src/day08-test.cpp
@@ -8,12 +8,12 @@ TEST(day08, part1)
 {
   std::string base = std::string(TXT_FILE_DIR);
   ASSERT_EQ(aoc::day08::part1(base + "day08/sample.txt"), 21);
-  ASSERT_EQ(aoc::day08::part1(base + "day08/input.txt"), 0);
+  ASSERT_EQ(aoc::day08::part1(base + "day08/input.txt"), 1717);
 }
 
 TEST(day08, part2)
 {
   std::string base = std::string(TXT_FILE_DIR);
-  ASSERT_EQ(aoc::day08::part2(base + "day08/sample.txt"), 0);
-  ASSERT_EQ(aoc::day08::part2(base + "day08/input.txt"), 0);
+  ASSERT_EQ(aoc::day08::part2(base + "day08/sample.txt"), 8);
+  ASSERT_EQ(aoc::day08::part2(base + "day08/input.txt"), 321975);
 }
